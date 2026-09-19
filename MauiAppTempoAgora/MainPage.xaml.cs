cfd1995a7009bad9d715dbb13421d0730b031183
RODRIGO MOLINARO <rodrigomolinaro@live.com>
2026-09-18 22:37:34 -0300

    Ag7

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace MauiAppTempoAgora
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+using System.ComponentModel.Design;
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
@@ -9,16 +13,45 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                var entrada = this.FindByName<Entry>("txt_cidade");
+                var lbl = this.FindByName<Label>("lbl_res");
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                string cidade = entrada?.Text ?? string.Empty;
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                if (!string.IsNullOrEmpty(cidade))
+                {
+                    Tempo? t = await DataService.GetPrevisao(cidade);
+
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
+
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                                        $"Longitude: {t.lon} \n" +
+                                        $"Nascer do Sol: {t.sunrise} \n" +
+                                        $"Pôr do Sol: {t.sunset} \n" +
+                                        $"Temp Máx: {t.temp_max} \n" +
+                                        $"Temp Min: {t.temp_min} \n";
+
+                        if (lbl != null)
+                            lbl.Text = dados_previsao;
+                    }
+                    else
+                    {
+                        if (lbl != null)
+                            lbl.Text = "Sem dados de Previsão";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
+
     }
 }
---
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -0,0 +1,53 @@
+﻿using MauiAppTempoAgora.Models;
+using Newtonsoft.Json.Linq;
+
+namespace MauiAppTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo? t = null;
+
+            string chave = "fe7010c3a91d8d8ec8538862e639d48b";
+
+            string url =
+                $"https://api.openweathermap.org/data/2.5/weather?" +
+                         $"q={cidade}&units=metric&appid={chave}";
+
+            using (HttpClient client = new HttpClient())
+            {
+
+                HttpResponseMessage resp = await client.GetAsync(url);
+
+                if(resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync();
+                    var rascunho = JObject.Parse(json);
+
+                    DateTime time = new();
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+
+                    t = new()
+                    {
+                        lon = (double)rascunho["coord"]["lon"],
+                        lat = (double)rascunho["coord"]["lat"],
+                        temp_min = (double)rascunho["main"]["temp_min"],
+                        temp_max = (double)rascunho["main"]["temp_max"],
+                        visibility = (int)rascunho["visibility"],
+                        speed = (double)rascunho["wind"]["speed"],
+                        main = (string)rascunho["weather"][0]["main"],
+                        description = (string)rascunho["weather"][0]["description"],
+                        sunrise = sunrise.ToString(),
+                        sunset = sunset.ToString(),
+                    }; //Fecha obj do tempo
+                } //Fecha if se o status do servidor foi de sucesso
+            } // fecha laço using 
+
+
+
+            return t;
+        }
+    }
+}
